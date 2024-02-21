96b0ce1c9f342469e5bae0fd2935c0d5be550fab
Humanshu <humanshujaglan@gmail.com>
2024-02-21 19:24:35 +0530

    added insights to the web

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Resume from './components/Resume';
 import Projects from './components/Projects';
 import $ from 'jquery';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 // import { useLocation } from 'react-router-dom';
 
 function App() {
@@ -60,6 +61,7 @@ function App() {
           <Route path="/contact" element={<Contact/>} />
           <Route path="/skills" element={<Skills/>} />
           <Route path='/resume' element={<Resume/>} />
+          <Route path="/admin/speed" element={<SpeedInsights/>} />
           <Route path="/stats" element={<Stats/>} />
         </Routes>
       </Router>
