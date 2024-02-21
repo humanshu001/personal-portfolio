4af1d9e2b1b88dd17e024f10b6a0df16bb755fb0
Humanshu <humanshujaglan@gmail.com>
2024-02-21 19:36:09 +0530

    text center in 404 not found line

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <>
         <div className="row skills m-3 mb-5" style={{backgroundColor:'var(--color-1)',borderRadius:'5px'}}>
-            <h1 className='my-4' style={{color:'var(--color-6)',fontWeight:'900',fontSize:'70px'}}>404 not found!</h1>
+            <h1 className='text-center my-4' style={{color:'var(--color-6)',fontWeight:'900',fontSize:'70px'}}>404 not found!</h1>
         </div>
 
     </>
