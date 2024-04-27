ab16fa826f33b8078fb6ee286ef82d13d0749844
Humanshu <humanshujaglan@gmail.com>
2024-04-27 22:27:36 +0530

    linked the projects section with github

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,39 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function Projects() {
+
+  const url = "https://api.github.com/users/humanshu001/repos";
+
+  const [repos, setRepos] = useState([]);
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => setRepos(data));
+
+
   return (
     <>
         <div className="row resume m-3" style={{backgroundColor:'var(--color-1)',borderRadius:'5px'}}>
             <div className="col-12 text-center">
-            <h1 className='my-4' style={{color:'var(--color-6)',fontWeight:'900',fontSize:'70px'}}>My Projects</h1>
+            <h1 className='my-4' style={{color:'var(--color-6)',fontWeight:'900',fontSize:'70px'}}>My Repositories</h1>
             </div>
             <div className="repos row d-flex justify-content-center m-auto">
-              <div className="card m-3" style={{width: '18rem',backgroundColor: 'var(--color-2)',border: 'none',margin: '10px'}}>
+              {repos.map((repo) => {
+                return (
+                  repo.visibility === 'public' && repo.name !== 'humanshu001' &&repo.name !== 'prograund_frontend' &&repo.name !== 'Bot-for-ProGraund' && repo.name !== 'Full-Stack-Web-Development-Course' &&
+               <div className="card m-3" style={{width: '18rem',backgroundColor: 'var(--color-2)',border: 'none',margin: '10px',padding:'20px'}}>
+                <p style={{color:'#0d74e7',fontSize:"19px",fontWeight:500}}>{repo.name}</p>
+                <p style={{color:'grey',fontSize:'13px'}}>{repo.description}</p>
+                <p style={{color:'grey',fontSize:'16px'}}><svg xmlns="http://www.w3.org/2000/svg" width="18" height="17" fill="#ffff00" class="bi bi-star-fill" viewBox="0 0 16 16">
+  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+</svg> {repo.stargazers_count}</p>
+               </div>
+                  
+                )
+              })
+              }
+              {/* <div className="card m-3" style={{width: '18rem',backgroundColor: 'var(--color-2)',border: 'none',margin: '10px'}}>
                 <div className="card-img-top" style={{borderBottom:'2px solid var(--color-1)'}}>
               <img className='w-100 m-auto mx-2 mb-3' src="https://github-readme-stats.vercel.app/api/pin/?username=humanshu001&repo=101-Hello-World-Programs&bg_color=282c34&hide_border=true" alt="" />
 
@@ -114,9 +139,8 @@ export default function Projects() {
                     My Old Portfolio website. The website is created using HTML, CSS, and JavaScript. The website is responsive and mobile-friendly.
                   </p>
                 </div>
-              </div>
+              </div> */}
 
-              
               
               
               
