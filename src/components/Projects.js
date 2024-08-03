88a38cdee33b3021356781b152b1173421424734
Humanshu <humanshujaglan@gmail.com>
2024-08-03 14:56:30 +0530

    some more modifications

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -25,7 +25,7 @@ export default function Projects() {
                <div className="card m-3 d-flex flex-column justify-content-between" style={{width: '18rem',backgroundColor: 'var(--color-2)',border: 'none',margin: '10px',padding:'20px'}}>
                 <p style={{color:'var(--color-3)',fontSize:"19px",fontWeight:500}}>{repo.name}</p>
                 <p style={{color:'var(--color-5)',fontSize:'13px'}}>{repo.description}</p>
-                <p className='d-flex align-items-center' style={{color:'grey',fontSize:'16px'}}><svg style={{marginRight:'8px',fill:'var(--color-3)'}} xmlns="http://www.w3.org/2000/svg" width="18" height="17" fill="#ffff00" class="bi bi-star-fill" viewBox="0 0 16 16">
+                <p className='d-flex align-items-center' style={{color:'var(--color-3)',fontSize:'16px'}}><svg style={{marginRight:'8px',fill:'var(--color-3)'}} xmlns="http://www.w3.org/2000/svg" width="18" height="17" fill="#ffff00" class="bi bi-star-fill" viewBox="0 0 16 16">
   <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
 </svg> {repo.stargazers_count}</p>
                </div>
